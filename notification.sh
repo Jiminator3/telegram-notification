e205a0b9c86edf22b9f6f8ad64e4dc2acb58423b
Timur Pospelov <timyrshow72@gmail.com>
2023-05-01 16:48:16 +0400

    Add comments.

--- notification.sh
+++ notification.sh
@@ -1,9 +1,11 @@
 #!bin/bash
 sleep 5 #Ожидание симулирующее выполнение продолжительной задачи.
 
+#Чтение текста из файла. TODO(Читать данные из log файла.)
 while read line
 do
 	text=$text+$line
 done < "${1:-/dev/stdin}"
 
+#Отправка уведомления о выполнении скрипта. TODO(Поменять ID бота и ID чата.)
 curl https://api.telegram.org/bot5885781455:AAH0qeZBgaZIUygeMEM0ErsOIpAmVbT59D8/sendMessage -d chat_id=444412919 -d text="$text"
